9b51853929e313f7e98043488313d4aca9b4510a
Lukas Gysin <Lu.Gysin@gmail.com>
2024-12-05 23:09:36 +0100

    feat: Navigation

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <nav className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="flex h-16 flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div className="flex flex-shrink-0 items-center">
+                        <h1 className="text-2xl font-bold text-white">
+                            Application Compass
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </nav>   
+    )
+}
