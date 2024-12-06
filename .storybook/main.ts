ddd3efd926ea9370f5299d9d5ae4659982102a87
Lukas Gysin <Lu.Gysin@gmail.com>
2024-12-06 14:47:39 +0100

    chore: Uninstall Visual Tests

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
