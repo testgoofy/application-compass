d1ecd3132fb13bdd7e4a47d1eabf240a616c65c5
Lukas Gysin <Lu.Gysin@gmail.com>
2024-12-09 10:17:44 +0100

    fix: Key in Loop

--- src/app/_components/dropdown.tsx
+++ src/app/_components/dropdown.tsx
@@ -6,7 +6,7 @@ export default function TextInput({id, title, options, value} : {id: string, tit
             <div className="flex px-3 items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
                 <select name={id} id={id} defaultValue={value} className="block min-w-0 grow py-1.5 bg-white text-base focus:outline focus:outline-0 sm:text-sm/6" >
                     {options.map((value) => (
-                        <option value={value}>{value}</option>
+                        <option key={value} value={value}>{value}</option>
                     ))}
                 </select>
             </div>
