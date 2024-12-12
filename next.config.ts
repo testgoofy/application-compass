aa061d2e2d1ec2eae7807cb3ac48f14832d8025f
Lukas Gysin <l.gysin@pm.me>
2024-12-12 10:49:12 +0100

    polling on devcontainer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if(process.env.USE_POLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300
+      }
+    }
+    return config
+  },
 };
 
 export default nextConfig;
