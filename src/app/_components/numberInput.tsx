2189f0581236497ae25e0a7fbcf210f4916bf62c
Lukas Gysin <l.gysin@pm.me>
2024-12-13 10:08:10 +0100

    feat: Add Styling Componentes

--- src/app/_components/numberInput.tsx
+++ src/app/_components/numberInput.tsx
@@ -1,7 +1,7 @@
-export default function NumberInput({id, title, value, placeholder, suffix} : {id: string, title: string, value?: number, placeholder?: string, suffix?: string}) {
+export default function NumberInput({id, title, value, placeholder, suffix, className} : {id: string, title: string, value?: number, placeholder?: string, suffix?: string, className?: string}) {
 
     return (
-        <div>
+        <div className={className}>
             <label htmlFor={id} className="pl-3 block text-sm/6 font-medium text-gray-900">{title}</label>
             <div className="flex px-3 items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
                 <input type="number" step={1} name={id} id={id} defaultValue={value} className="mr-2 block min-w-0 grow py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder={placeholder} />
--- src/app/_components/textArea.tsx
+++ src/app/_components/textArea.tsx
@@ -1,7 +1,7 @@
-export default function TextField({id, title, value} : {id: string, title: string, value?: string}) {
+export default function TextField({id, title, value, className} : {id: string, title: string, value?: string, className?: string}) {
 
     return (
-        <div>
+        <div className={className}>
             <label htmlFor={id} className="pl-3 block text-sm/6 font-medium text-gray-900">{title}</label>
             <div className="flex px-3 items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
                 <textarea name={id} id={id} defaultValue={value} className="block min-w-0 grow py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 h-56" />
