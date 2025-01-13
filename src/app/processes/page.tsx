448031fdb0b6af116277289fc60d51338b649c8a
Lukas Gysin <l.gysin@pm.me>
2025-01-13 09:17:31 +0100

    feat: Process placeholder page

---
+++ src/app/processes/page.tsx
@@ -0,0 +1,11 @@
+export default function Processes() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">
+          Processes
+        </h1>
+      </div>
+    </div>
+  );
+}
