210c2481cef8902764f7045478eda239859116b0
Lukas Gysin <l.gysin@pm.me>
2025-01-31 09:14:52 +0100

    feat: Process Name

--- src/app/processes/page.tsx
+++ src/app/processes/page.tsx
@@ -1,4 +1,14 @@
-export default function Processes() {
+import type { Process } from "@prisma/client";
+import client from "@/app/_logic/database";
+
+export default async function Processes() {
+
+  const processes = await client.process.findMany({
+    include: {
+      processNodes: true
+    }
+  });
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex justify-between">
@@ -6,6 +16,11 @@ export default function Processes() {
           Processes
         </h1>
       </div>
+      <ul className="divide-y divide-gray-200 border border-gray-300 rounded-lg">
+            {processes.map((process: Process) => (
+                <p>{ process.name }</p>
+            ))}
+        </ul>
     </div>
   );
 }
